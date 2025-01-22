0eaf60a72ccb4d166bb1901036b294ffab6f2b39
YanisAF <yanissf2@gmail.com>
2025-01-22 16:49:16 +0100

    add jwtService

--- src/main/java/org/example/exercice11_springwebfluxsecurity/SecurityConfig.java
+++ src/main/java/org/example/exercice11_springwebfluxsecurity/SecurityConfig.java
@@ -1,9 +1,43 @@
 package org.example.exercice11_springwebfluxsecurity;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    private final JwtService jwtService;
+
+    public SecurityConfig(final JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        AuthenticationWebFilter jwtWebFilter = new AuthenticationWebFilter(authentication());
+        jwtWebFilter.setServerAuthenticationConverter(new JwtAuthentificationFilter(jwtService));
+
+        return http
+                .authorizeExchange()
+                .pathMatchers("")
+    }
+
+    @Bean
+    public ReactiveAuthenticationManager authenticationManager(){
+        return authentication -> {
+            if (authentication instanceof UsernamePasswordAuthenticationToken) {
+                return Mono.just(authentication);
+            }
+            return Mono.empty();
+        };
+    }
+
+
 }
