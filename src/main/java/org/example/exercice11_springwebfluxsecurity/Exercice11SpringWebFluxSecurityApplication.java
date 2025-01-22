6fcb5af6d64b0b1e42a578f9a7c3efedc28350e4
YanisAF <yanissf2@gmail.com>
2025-01-22 16:41:38 +0100

    add exercice11 springwebfluxsecurity

---
+++ src/main/java/org/example/exercice11_springwebfluxsecurity/Exercice11SpringWebFluxSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.exercice11_springwebfluxsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercice11SpringWebFluxSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercice11SpringWebFluxSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/exercice11_springwebfluxsecurity/Exercice11SpringWebFluxSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exercice11_springwebfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercice11SpringWebFluxSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
